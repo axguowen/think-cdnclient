aa4f1f23897cad7f5bd4a8b7706eb2c203762eba
axguowen <axguowen@qq.com>
2024-12-10 12:25:31 +0800

    修正

--- src/cdnclient/driver/Aliyun.php
+++ src/cdnclient/driver/Aliyun.php
@@ -56,6 +56,220 @@ class Aliyun extends Platform
     }
 
     /**
+     * 新增加速域名
+     * @access public
+     * @param string $domain
+     * @return array
+     */
+    public function addCdnDomain(string $domain)
+    {
+        return [null, new \Exception('暂不支持该方法')];
+    }
+
+    /**
+	 * 查询域名归属权解析记录信息
+	 * @access public
+	 * @param string $domain
+	 * @return array
+	 */
+	public function getRecord(string $domain)
+    {
+        return [null, new \Exception('暂不支持该方法')];
+    }
+
+    /**
+	 * 验证域名归属权
+	 * @access public
+	 * @param string $domain
+	 * @return array
+	 */
+	public function verifyRecord(string $domain)
+    {
+        return [null, new \Exception('暂不支持该方法')];
+    }
+
+    /**
+	 * 删除域名
+	 * @access public
+	 * @param string $domain
+	 * @return array
+	 */
+	public function deleteCdnDomain(string $domain)
+    {
+        return [null, new \Exception('暂不支持该方法')];
+    }
+
+    /**
+	 * 设置IP黑名单
+	 * @access public
+	 * @param string $domain
+	 * @param array $ipList
+	 * @return array
+	 */
+	public function setIpBlackList(string $domain, array $ipList = [])
+    {
+        return [null, new \Exception('暂不支持该方法')];
+    }
+
+    /**
+	 * 设置UA黑名单
+	 * @access public
+	 * @param string $domain
+	 * @param array $uaList
+	 * @return array
+	 */
+	public function setUaBlackList(string $domain, array $uaList = [])
+    {
+        return [null, new \Exception('暂不支持该方法')];
+    }
+
+    /**
+	 * 设置域名证书
+	 * @access public
+	 * @param string $domain
+	 * @param array $certificate
+	 * @return array
+	 */
+	public function setCertificate(string $domain, array $certificate)
+    {
+        return [null, new \Exception('暂不支持该方法')];
+    }
+
+    /**
+	 * 查询域名证书
+	 * @access public
+	 * @param string $domain
+	 * @return array
+	 */
+	public function getCertificate(string $domain)
+    {
+        return [null, new \Exception('暂不支持该方法')];
+    }
+
+    /**
+	 * 删除域名证书
+	 * @access public
+	 * @param string $domain
+	 * @return array
+	 */
+	public function deleteCertificate(string $domain)
+    {
+        return [null, new \Exception('暂不支持该方法')];
+    }
+
+    /**
+	 * 删除域名证书
+	 * @access public
+	 * @param string $certName
+	 * @return array
+	 */
+	public function destroyCertificate(string $certName)
+    {
+        return [null, new \Exception('暂不支持该方法')];
+    }
+
+    /**
+	 * 启用加速域名
+	 * @access public
+	 * @param string $domain
+	 * @return array
+	 */
+	public function startCdnDomain(string $domain)
+    {
+        return [null, new \Exception('暂不支持该方法')];
+    }
+
+    /**
+	 * 停用加速域名
+	 * @access public
+	 * @param string $domain
+	 * @return array
+	 */
+	public function stopCdnDomain(string $domain)
+    {
+        return [null, new \Exception('暂不支持该方法')];
+    }
+
+    /**
+	 * 设置缓存规则
+	 * @access public
+	 * @param string $domain
+	 * @return array
+	 */
+	public function setCacheRules(string $domain, array $cacheRules)
+    {
+        return [null, new \Exception('暂不支持该方法')];
+    }
+
+    /**
+	 * 设置IP访问限频
+	 * @access public
+	 * @param string $domain
+	 * @param int $qps
+	 * @param int $timeRange
+	 * @return array
+	 */
+	public function setAccessLimit(string $domain, int $qps = 0, int $timeRange = 1)
+    {
+        return [null, new \Exception('暂不支持该方法')];
+    }
+
+	/**
+	 * 设置回源请求头
+	 * @access public
+	 * @param string $domain
+	 * @param array $header
+	 * @return array
+	 */
+	public function setRequestHeader(string $domain, array $header)
+    {
+        return [null, new \Exception('暂不支持该方法')];
+    }
+
+    /**
+	 * 刷新文件缓存
+	 * @access public
+	 * @param array|string $urls
+	 * @return array
+	 */
+	public function purgeUrlsCache($urls = [])
+    {
+        return [null, new \Exception('暂不支持该方法')];
+    }
+
+    /**
+	 * 刷新目录缓存
+	 * @access public
+	 * @param array|string $path
+	 * @return array
+	 */
+	public function purgePathsCache($paths = [])
+    {
+        return [null, new \Exception('暂不支持该方法')];
+    }
+
+    /**
+	 * 查询缓存刷新状态
+	 * @access public
+	 * @param string $taskId
+	 * @return array
+	 */
+	public function getPurgeStatus($taskId)
+    {
+        return [null, new \Exception('暂不支持该方法')];
+    }
+
+    /**
+	 * 查询缓存刷新限额
+	 * @access public
+	 * @return array
+	 */
+	public function getPurgeQuota()
+    {
+        return [null, new \Exception('暂不支持该方法')];
+    }
+
+    /**
 	 * 查询域名备案状态
 	 * @access public
 	 * @param string $domain
